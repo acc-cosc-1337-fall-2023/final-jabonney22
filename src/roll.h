cb94d220cc48c42ba65c5bac01f10fb8aa524591
jabonney22 <66222611+jabonney22@users.noreply.github.com>
2023-12-11 03:18:45 +0000

    Question 2

--- src/roll.h
+++ src/roll.h
@@ -1,1 +1,20 @@
 //
+#include "die.h"
+
+#ifndef ROLL_H
+#define ROLL_H
+
+class Roll
+{
+    public:
+        Roll(Die& d1, Die& d2) : die1(d1), die2(d2){/*empty class function*/}
+        void roll_die();
+        int roll_value() const{return value;}
+    private:
+        Die& die1;
+        Die& die2;
+        bool rolled = false;
+        int value;
+};
+#endif
+
--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "die.h"
+#include "roll.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -23,3 +24,21 @@ TEST_CASE("Testing die roll class function")
 	}
 }
 
+TEST_CASE("Testing roll roll class functions") 
+{
+	srand((unsigned) time(NULL));
+	Die dice1;
+	Die dice2;
+	Roll roll(dice1,dice2);
+	roll.roll_die();
+	REQUIRE(roll.roll_value() >= 2);
+	REQUIRE(roll.roll_value() <= 12);
+
+	for(int i = 0; i < 10; i++)
+	{
+		roll.roll_die();
+		std::cout<<roll.roll_value()<<"\n";
+		REQUIRE(roll.roll_value() >= 2);
+		REQUIRE(roll.roll_value() <= 12);
+	}
+}
