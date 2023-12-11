b0bf25aa1b06c28036f26a14d991d4bd16392f00
jabonney22 <66222611+jabonney22@users.noreply.github.com>
2023-12-11 03:28:30 +0000

    Question 4

--- src/come_out_phase.h
+++ src/come_out_phase.h
@@ -1,1 +1,16 @@
 //
+#include<iostream>
+#include "phase.h"
+
+#ifndef COME_OUT_PHASE_H
+#define COME_OUT_PHASE_H
+
+
+
+class ComeOutPhase : public Phase
+{
+    public:
+        RollOutcome get_outcome(Roll* roll) override;
+
+};
+#endif
--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -3,6 +3,8 @@
 #include "die.h"
 #include "roll.h"
 #include "shooter.h"
+#include "come_out_phase.h"
+#include "point_phase.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -61,4 +63,54 @@ TEST_CASE("Testing shooter throw die class functions")
 		REQUIRE(roll->roll_value() <= 12);
 	}
 
+}TEST_CASE("Testing ComeOutPhase class")
+{
+	srand((unsigned) time(NULL));
+	ComeOutPhase comeoutphase;
+	Die dice1;
+	Die dice2;
+	Shooter shooter;
+	Roll* roll = shooter.throw_die(dice1,dice2);
+	for(int i = 0; i < 10; i++)
+	{
+		roll->roll_die();
+		if(roll->roll_value() == 7 || roll->roll_value() == 11)
+		{
+			REQUIRE(comeoutphase.get_outcome(roll) == RollOutcome::natural);
+		}
+		else if(roll->roll_value() == 2 || roll->roll_value() == 3 || roll->roll_value() == 12)
+		{
+			REQUIRE(comeoutphase.get_outcome(roll) == RollOutcome::craps);
+		}
+		else
+		{
+			REQUIRE(comeoutphase.get_outcome(roll) == RollOutcome::point);
+		}
+	}
+}
+
+TEST_CASE("Testing PointPhase class")
+{
+	srand((unsigned) time(NULL));
+	PointPhase pointphase(4);
+	Die dice1;
+	Die dice2;
+	Shooter shooter;
+	Roll* roll = shooter.throw_die(dice1,dice2);
+	for(int i = 0; i < 10; i++)
+	{
+		roll->roll_die();
+		if(roll->roll_value() == 4)
+		{
+			REQUIRE(pointphase.get_outcome(roll) == RollOutcome::point);
+		}
+		else if(roll->roll_value() == 7)
+		{
+			REQUIRE(pointphase.get_outcome(roll) == RollOutcome::seven_out);
+		}
+		else
+		{
+			REQUIRE(pointphase.get_outcome(roll) == RollOutcome::nopoint);
+		}
+	}
 }
